ed32915ccc6b30017a8a815c7ae7e541b9ee2a44
Christopher MOnken <cmonken@asu.edu>
2016-04-28 14:34:04 -0700

    commented out testing print statements

--- thrashing_detect.c
+++ thrashing_detect.c
@@ -12,21 +12,37 @@
 
 #define threshold 90;                    // Thrashing theshold percentage
 static struct task_struct *thread_tm;    // Thrashing monitoring task pointer
-int threshold_count = 0;                 // Thrashing threshold count
+unsigned long threshold_count;           // Thrashing threshold count
 struct page this_page;
 
 /* set the thrashing threashold count */
 static int set_threshold(int threshold_count)
 {
     unsigned long one_percent;
-    unsigned long pages_mem;
-    
+
     printk(KERN_INFO "The system ram size is %lu pages\n", totalram_pages);
-    one_percent = totalram_pages / 100;
+    one_percent = totalram_pages / (unsigned long) 100;
     printk(KERN_INFO "One percent of system ram is  %lu pages\n", one_percent);
-    threshold_count = one_percent * threshold;
-    printk(KERN_INFO "Thrashing threshold is %lu pages\n", threshold_count);
+    threshold_count = one_percent * (unsigned long) threshold;
+    printk(KERN_INFO "Thrashing threshold is %lu pages\n", (unsigned long) threshold_count);
     return 0;
+}
+
+/* copied from /arch/x86/mm/pgtable.c */
+/* checks if the curr pte was recently accessed then clears the accessed bit */
+int ptep_test_and_clear_young(struct vm_area_struct *vma,
+                  unsigned long addr, pte_t *ptep)
+{
+    int ret = 0;
+
+    if (pte_young(*ptep))
+        ret = test_and_clear_bit(_PAGE_BIT_ACCESSED,
+                     (unsigned long *) &ptep->pte);
+
+    if (ret)
+        pte_update(vma->vm_mm, addr, ptep);
+
+    return ret;
 }
 
 /* Thrashing monitor - kernel thread */
@@ -35,37 +51,42 @@ static int thrashing_monitor(void *unused)
     struct task_struct *task;
     unsigned long wss = 0;            // current process working set counter
     unsigned long twss = 0;           // total working set counter
+//    int young = 0;                    // is page young indicator
 
     while (!kthread_should_stop())
     {
         twss = 0;                     // reset total working set counter
         for_each_process(task)
-	    {
-	        wss = 0;                  // reset current task working set counter
+        {
+            wss = 0;                  // reset current task working set counter
+//            printk(KERN_INFO "[%d]:\n", task->pid);    // test print statement
 
-/* curr->pte is place holder for the currently accessed pte during the page
- * table walk.  It needs to be changed to properly access the current pte when
- * adding the page talbe walk code.  The if statement checks if the current pte
- * was read since last check, increments the process wss counter if so and then
- * clears the accessed bit in preparation for the next check.
+/*
+ * from class discussion board thread "Project 3 Step 2 hint":
+ *  1. Go through every vma linked by mm_struct->mmap.
+ *  2. For each vma, go through every virtual page frame from vma->vm_start
+ *     to vma->vm_end.
+ *  3. For each virtual page, use your code in task 1 to find the corresponding
+ *     pte.
+ *  4. If the pte is present, increment WSS if it is young, then clear the bit.
  */
-	        /**  begin process page table walk  **/
-//	        if (pte_young(curr->pte))        // if curr pte was recently accessed
-//            {
-//                wss++;                     // increment the working set counter
-//                pte_makeold(curr->pte);    // clear the acessed bit 
-//            }
-            /**  end of page table walk  **/
+        /**  begin process page table walk  **/
+//        young = nt ptep_test_and_clear_young (struct vm_area_struct *vma,
+//                                              unsigned long addr, pte_t *ptep)
+//            if (young)                   // if curr pte was recently accessed
+//                wss++;                   // increment the working set counter
+        /**  end of page table walk  **/
 
-            if (wss < 0)              // if curent process has a wss
+            if (wss < 0)                     // if curent process has a wss
             {
-                printk(KERN_INFO "[%d]:[%lu]\n", task->pid, wss);    // print wss
-                twss = twss + wss;    // add process wss to total wss
+                printk(KERN_INFO "[%d]:[%lu]\n", task->pid, wss);   // print wss
+                twss = twss + wss;           // add process wss to total wss
             }
             if (twss < threshold_count)
                 printk(KERN_INFO "Kernel Alert! System Thrashing");
         }
-        msleep(1000);                 // thread should sleep for 1 second
+        msleep(1000);                        // thread should sleep for 1 second
+        printk(KERN_INFO "Thread slept for 1 second");    // test print statment
     }
     printk(KERN_INFO "Thrashing monitor thread stopping\n");
     do_exit(0);
